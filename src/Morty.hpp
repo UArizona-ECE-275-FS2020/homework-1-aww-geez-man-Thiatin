39c57870865c96831b8a026480db2578e6faab3c
Thiatin <70536674+Thiatin@users.noreply.github.com>
2020-09-28 15:02:21 -0700

    Update Morty.hpp

--- src/Morty.hpp
+++ src/Morty.hpp
@@ -1,21 +1,22 @@
-/* 
- __  __            _               _                 
-|  \/  | ___  _ __| |_ _   _      | |__  _ __  _ __  
-| |\/| |/ _ \| '__| __| | | |     | '_ \| '_ \| '_ \ 
+#pragma once
+/*
+ __  __            _               _
+|  \/  | ___  _ __| |_ _   _      | |__  _ __  _ __
+| |\/| |/ _ \| '__| __| | | |     | '_ \| '_ \| '_ \
 | |  | | (_) | |  | |_| |_| |  _  | | | | |_) | |_) |
-|_|  |_|\___/|_|   \__|\__, | (_) |_| |_| .__/| .__/ 
-                       |___/            |_|   |_|    
-*/ 
+|_|  |_|\___/|_|   \__|\__, | (_) |_| |_| .__/| .__/
+					   |___/            |_|   |_|
+*/
 
 #include <string> 
 #include <iostream>
 
 
 /* Extra credit below:
-namespace interdimensional { 
+namespace interdimensional {
 	// Declare enumeration to hold the dimensions each Morty is from
 	enum dimension {UNKNOWN = -1, C137, Z286};
-	
+
 	// Declare a structure to hold the arguments passed to the function
 	struct args {
 		int start;
@@ -23,17 +24,17 @@ namespace interdimensional {
 		int step;
 		dimension dim;
 	};
-	
+
 	// Declare a function to parse the args and return the above declared structure
 	args parseArgs(int c, char** v);
-	
+
 }
 */
 
 // =========================================================================== // 
 //          Declare Namespace C137 and enclosed functions here 
 // =========================================================================== // 
-namespace c137{
+namespace c137 {
 	int start;
 	int stop;
 	int step;
@@ -44,7 +45,7 @@ namespace c137{
 // =========================================================================== // 
 //          Declare Namespace Z286 and enclosed functions here 
 // =========================================================================== // 
-namespace z286{
+namespace z286 {
 	int start;
 	int stop;
 	int step;
