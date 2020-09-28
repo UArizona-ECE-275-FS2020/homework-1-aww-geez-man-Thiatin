3c4a7e71eb06d70584d320c2e184d3589067252a
Thiatin <70536674+Thiatin@users.noreply.github.com>
2020-09-28 15:02:38 -0700

    Update Morty.cpp

--- src/Morty.cpp
+++ src/Morty.cpp
@@ -1,10 +1,10 @@
-/* 
- __  __            _                                
-|  \/  | ___  _ __| |_ _   _        ___ _ __  _ __  
-| |\/| |/ _ \| '__| __| | | |      / __| '_ \| '_ \ 
+/*
+ __  __            _
+|  \/  | ___  _ __| |_ _   _        ___ _ __  _ __
+| |\/| |/ _ \| '__| __| | | |      / __| '_ \| '_ \
 | |  | | (_) | |  | |_| |_| |  _  | (__| |_) | |_) |
-|_|  |_|\___/|_|   \__|\__, | (_)  \___| .__/| .__/ 
-                       |___/           |_|   |_|  
+|_|  |_|\___/|_|   \__|\__, | (_)  \___| .__/| .__/
+                       |___/           |_|   |_|
 */
 
 // Include the Morty header file
@@ -14,90 +14,90 @@
 // =========================================================================== // 
 //          Define functions enclosed in Namespace C137 here 
 // =========================================================================== // 
-using namespace c137{
+namespace c137 {
 
- void Morty(int start,int stop){
- 
-  check = 0;
- 
-  while (start <= stop) {
-  
-  if (check == (start %3){
-   printf("Aww");
-  }
-  
-  if (check == (start%5) {
-   printf("Geez");
-  }
-  
-  if (check == (start%15) {
-   printf("Man");
-  }
-  start++;
-  printf("\n");
- } //while bracket
-} //function bracket
- 
- void Morty(int start, int stop, int step) {
-  check = 0;
-  while (start <= stop){
-  if (check == (start %3){
-   printf("Aww");
-  }
-  
-  if (check == (start%5) {
-   printf("Geez");
-  }
-  
-  if (check == (start%15) {
-   printf("Man");
-  }
-  start= start + step;
-  printf("\n");
-  } //while bracket
- } //function bracket
+    void Morty(int start, int stop) {
+
+        check = 0;
+
+        while (start <= stop) {
+
+            if (check == (start % 3)) {
+                printf("Aww");
+            }
+
+            if (check == (start % 5)) {
+                printf("Geez");
+            }
+
+            if (check == (start % 15)) {
+                printf("Man");
+            }
+            start++;
+                printf("\n");
+        } //while bracket
+    } //function bracket
+
+    void Morty(int start, int stop, int step) {
+        check = 0;
+            while (start <= stop) {
+                if (check == (start % 3)) {
+                    printf("Aww");
+                }
+
+                if (check == (start % 5)) {
+                    printf("Geez");
+                }
+
+                if (check == (start % 15)) {
+                    printf("Man");
+                }
+                start = start + step;
+                    printf("\n");
+            } //while bracket
+    } //function bracket
 } //namespace bracket
 // =========================================================================== // 
 //          Define functions enclosed in Namespace Z286 here 
 // =========================================================================== // 
-using namespace z286{
- void Morty(int start,int stop){
- check = 0;
- while (start <= stop) {
-  if (check == (start %3){
-   printf("wwA");
-  }
-  
-  if (check == (start%5) {
-   printf("zeeG");
-  }
-  
-  if (check == (start%15) {
-   printf("naM");
-  }
-  start++;
-  printf("\n");
- }//while bracket
-}//function bracket
- 
- void Morty(int start, int stop, int step) {
-  check = 0;
-  while (start <= stop){
-  if (check == (start %3){
-   printf("wwA");
-  }
-  
-  if (check == (start%5) {
-   printf("zeeG");
-  }
-  
-  if (check == (start%15) {
-   printf("naM");
-  }
-  start= start + step;
-  printf("\n");
-  } //while bracket
- }//function bracket
+ namespace z286 {
+    void Morty(int start, int stop) {
+        check = 0;
+        while (start <= stop) {
+            if (check == (start % 3)) {
+                printf("wwA");
+            }
+
+            if (check == (start % 5)) {
+                printf("zeeG");
+            }
+
+            if (check == (start % 15)) {
+                printf("naM");
+            }
+            start++;
+                printf("\n");
+        }//while bracket
+    }//function bracket
+
+    void Morty(int start, int stop, int step) {
+        check = 0;
+            while (start <= stop) {
+                if (check == (start % 3)) {
+                    printf("wwA");
+                }
+
+                if (check == (start % 5)) {
+                    printf("zeeG");
+                }
+
+                if (check == (start % 15)) {
+                    printf("naM");
+                }
+                start = start + step;
+                    printf("\n");
+            } //while bracket
+    }//function bracket
 
 
 }//namespace bracket
