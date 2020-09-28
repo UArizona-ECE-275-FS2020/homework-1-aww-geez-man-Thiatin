925d2d23823b5b65d617f0d25e92f0d66d7aaef8
Thiatin <70536674+Thiatin@users.noreply.github.com>
2020-09-28 15:02:01 -0700

    Update AwwGeezMan.cpp

--- src/AwwGeezMan.cpp
+++ src/AwwGeezMan.cpp
@@ -1,33 +1,31 @@
 /*
- _____ ____ _____   ____ _____ ____  
-| ____/ ___| ____| |___ \___  | ___| 
-|  _|| |   |  _|     __) | / /|___ \ 
+ _____ ____ _____   ____ _____ ____
+| ____/ ___| ____| |___ \___  | ___|
+|  _|| |   |  _|     __) | / /|___ \
 | |__| |___| |___   / __/ / /  ___) |
-|_____\____|_____| |_____/_/  |____/ 
-                                     
- _   _                                         _      _ 
+|_____\____|_____| |_____/_/  |____/
+
+ _   _                                         _      _
 | | | | ___  _ __ ___   _____      _____  _ __| | __ / |
 | |_| |/ _ \| '_ ` _ \ / _ \ \ /\ / / _ \| '__| |/ / | |
 |  _  | (_) | | | | | |  __/\ V  V / (_) | |  |   <  | |
 |_| |_|\___/|_| |_| |_|\___| \_/\_/ \___/|_|  |_|\_\ |_|
- 
- 
-Aww Geez Man: Interdimensional Mortys.
 
+
+Aww Geez Man: Interdimensional Mortys.
 One of Morty C137's favorite phrases is "Aww Geez Man". But, in dimension Z286, there
 exists a Morty who says everything backwards.
-
 */
 #include <stdlib.h>
 #include <string.h>
-#include <iostream.h>
+#include <iostream>
 
 // Include the Morty header file
 #include "Morty.hpp"
 #include "Morty.cpp"
 
 
-int main (int ac, char** av) {
+int main(int ac, char** av) {
 	char dimension[4];
 	int start = 0;
 	int stop = 0;
@@ -37,57 +35,57 @@ int main (int ac, char** av) {
 	// or 
 	// ./AwwGeezMan {start} {stop} {step} {dimension}
 	if (ac != 4 and ac != 5) {
-		std::cout << "Error: Command line arguments are incorrect. Call program as (1) or (2)" 
+		std::cout << "Error: Command line arguments are incorrect. Call program as (1) or (2)"
 			<< std::endl;
 		std::cout << "(1)\t./AwwGeezMan {start} {stop} {dimension}" << std::endl;
 		std::cout << "(2)\t./AwwGeezMan {start} {stop} {step} {dimension}" << std::endl;
-		
+
 		return -1;
 	}
 	else if (ac == 4) {
-		start = argv[1];
-		stop = argv[2];
-		strcpy(dimension, argv[3]);
-		
+		start = atoi(*(av + 1));
+		stop = atoi(*(av + 2));
+		strcpy(dimension, av[3]);
+
 		if (dimension == "C137") {
-		using namespace c137;
+			using namespace c137;
 			Morty(start, stop);
 		}
 		else if (dimension == "Z286") {
-		using namespace z286;
+			using namespace z286;
 			Morty(start, stop);
 		}
 		else {
-		std::cout<<"Dimension not Recognized"<<std::endl;
-		std::cout<<"Dimensions are C137 or Z286"<<std::endl;
+			std::cout << "Dimension not Recognized" << std::endl;
+			std::cout << "Dimensions are C137 or Z286" << std::endl;
 		}
 	}
 	else if (ac == 5) {
-		start = argv[1];
-		stop = argv[2];
-		step = argv[3]
-		strcpy(dimension, argv[4]);
-		
+		start = atoi(*(av + 1));
+		stop = atoi(*(av + 2));
+		step = atoi(*(av + 3));
+		strcpy(dimension, av[4]);
+
 		if (dimension == "C137") {
-		using namespace c137;
+			using namespace c137;
 			Morty(start, stop, step);
 		}
 		else if (dimension == "Z286") {
-		using namespace z286;
+			using namespace z286;
 			Morty(start, stop, step);
 		}
 		else {
-		std::cout<<"Dimension not Recognized"<<std::endl;
-		std::cout<<"Dimensions are C137 or Z286"<<std::endl;
+			std::cout << "Dimension not Recognized" << std::endl;
+			std::cout << "Dimensions are C137 or Z286" << std::endl;
 		}
 	}
-	
+
 	// Parse the command line arguments
-	
-		
+
+
 	// Depending on the dimension of the arguments, call the appropriate Morty
 	// function
-	
-	
+
+
 	return 0;
 }
